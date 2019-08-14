67fbb6a01ba18e84d355399d0d01675deaccae9b
Eugen Guriev <gurievcreative@gmail.com>
2019-08-14 14:37:47 +0300

    init

---
+++ Data/Path.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LolitaFramework\Data;
+
+use \LolitaFramework\Data\Str;
+
+/**
+ * Class for working with path
+ */
+abstract class Path extends Str {
+
+	/**
+	 * Join path pieces
+	 *
+	 * @param  array  $array     pieces.
+	 * @param  string $separator directory separator.
+	 * @return string
+	 */
+	public static function join( $array = array(), $separator = '/' ) {
+		return implode( $separator, $array );
+	}
+
+	/**
+	 * Is path have extension?
+	 *
+	 * @param  string $path file.
+	 * @return boolean YES / NO.
+	 */
+	public static function is_have_extension( $path ) {
+		return array_key_exists( 'extension', pathinfo( $path ) );
+	}
+}
